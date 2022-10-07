217b76288332e4119a8f10b4fda21b8175d00198
Shivansh-soni <shivanshsoni.indore@gmail.com>
2022-10-07 14:16:16 +0530

    Cleared the Clutter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import Line from "./Line";
-import Mtext from "./Mtext";
 import Scroll from "./Scroll/Scroll";
-import Temp from "./Temp";
 
 function App() {
   return (
     <div>
-      {/* <Temp /> */}
-      {/* <Mtext /> */}
-      {/* <Line /> */}
       <Scroll />
     </div>
   );
